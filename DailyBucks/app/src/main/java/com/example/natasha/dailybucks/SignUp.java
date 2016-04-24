fa2d52baeeaa05227ab0ed8fd558503008efa384
cipherjai <jaiatmart@gmail.com>
2016-04-24 21:15:03 +0530

    Added Sign Up

--- DailyBucks/app/src/main/java/com/example/natasha/dailybucks/SignUp.java
+++ DailyBucks/app/src/main/java/com/example/natasha/dailybucks/SignUp.java
@@ -1,13 +1,28 @@
 package com.example.natasha.dailybucks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
+
+    private static TextView texty;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        OnClickListener();
     }
+    public void OnClickListener(){
+        texty = (TextView) findViewById(R.id.textView19);
+        texty.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getBaseContext(), Login.class);
+                startActivity(intent);
+            }
+        });
 }
